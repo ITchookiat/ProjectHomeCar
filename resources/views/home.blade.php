d7bbc20d6ff4e6ff12b45fde724dade9865a8d84
Saudear <final.fantasyxii@outlook.com>
2021-01-26 08:31:35 +0700

    update code

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,129 +2,107 @@
 @section('title','Home')
 @section('content')
 
-    <div class="content-header">
-        <div class="row justify-content-center">
-            <div class="col-md-12 table-responsive">
-                <div class="card">
-                    <div class="card-body">
-                        @if (session('status'))
-                            <div class="alert alert-success" role="alert">
-                                {{ session('status') }}
-                            </div>
-                        @endif
-                        <div class="row">
-                            <div class="col-6">
-                              <div class="form-inline">
-                                <h1 class="m-0 text-dark">Chookiat HomeCar</h1>
-                              </div>
-                            </div>
-                            <div class="col-6">
-                                <a class="btn bg-danger btn-app float-right" data-toggle="modal" data-target="#modal-1" data-link="{{ route('datacar', 13) }}" data-backdrop="static" data-keyboard="false" style="border-radius: 10px;">
-                                    <span class="fas fa-balance-scale prem fa-5x"></span> <label class="prem">เทียบราคา</label>
-                                </a>
-                            </div>
-                        </div>
-                        {{-- <div class="row mb-0">
-                            <div class="col-sm-6">
-                              <h1 class="m-0 text-dark">LAB Leasing</h1>
-                            </div>
-                            <div class="col-sm-6">
-                                <a class="btn bg-danger btn-app float-right" data-toggle="modal" data-target="#modal-1" data-link="{{ route('datacar', 13) }}" data-backdrop="static" data-keyboard="false" style="border-radius: 10px;">
-                                    <span class="fas fa-balance-scale prem fa-5x"></span> <label class="prem">เทียบราคา</label>
-                                </a>
-                            </div>
-                        </div> --}}
+    @if (session('status'))
+        <div class="alert alert-success" role="alert">
+            {{ session('status') }}
+        </div>
+    @endif
 
-                        <div align="center">
-                            <img class="img-responsive" src="{{ asset('dist/img/homecar.png') }}" alt="User Image" style = "width: 40%">
-                        </div>
+    <div class="card">
+        <div class="card-header">
+            <div class="pricing-header px-3 py-3 pt-md-3 pb-md-0 mx-auto text-center">
+                <div class="card-tools">
+                    <a class="btn bg-danger btn-app float-right" data-toggle="modal" data-target="#modal-1" data-link="{{ route('datacar', 13) }}" data-backdrop="static" data-keyboard="false" style="border-radius: 10px;">
+                        <span class="fas fa-balance-scale prem fa-5x"></span> <label class="prem">เทียบราคา</label>
+                    </a>
+                </div>
+                {{-- <div align="center">
+                    <img class="img-responsive" src="{{ asset('dist/img/homecar.png') }}" alt="User Image" style = "width: 40%">
+                </div> --}}
+            </div>
+        </div>
+        <div class="card-body">
+            <div class="row">
+                <div class="col-lg-1 col-md-6"></div>
+                <div class="col-lg-3 col-6">
+                    <div class="small-box bg-info">
+                    <div class="inner">
+                        <h3>{{$data1-$data6}}</h3>
+                        <p>รถยนต์ทั้งหมด</p>
+                    </div>
+                    <div class="icon">
+                        <i class="fa fa-car fa-5x"></i>
+                    </div>
+                        <a href="{{ route('datacar', 1) }}" class="small-box-footer">ดูเพิ่มเติม <i class="fas fa-arrow-circle-right"></i></a>
+                    </div>
+                </div>
+                <div class="col-lg-4 col-6">
+                    <div class="small-box bg-purple">
+                    <div class="inner">
+                        <h3>{{$data2}}</h3>
+                        <p>รถยนต์ระหว่างทำสี</p>
+                    </div>
+                    <div class="icon">
+                        <i class="fa fa-paint-brush fa-5x"></i>
+                    </div>
+                        <a href="{{ route('datacar', 2) }}" class="small-box-footer">ดูเพิ่มเติม <i class="fas fa-arrow-circle-right"></i></a>
+                    </div>
+                </div>
+                <div class="col-lg-3 col-6">
+                    <div class="small-box bg-navy">
+                    <div class="inner">
+                        <h3>{{$data3}}</h3>
+                        <p>รถยนต์รอซ่อม</p>
+                    </div>
+                    <div class="icon">
+                    <i class="fas fa-car-crash"></i>
+                        <!-- <i class="fa fa-exclamation-circle fa-5x"></i> -->
+                    </div>
+                        <a href="{{ route('datacar', 3) }}" class="small-box-footer">ดูเพิ่มเติม <i class="fas fa-arrow-circle-right"></i></a>
+                    </div>
+                </div>
+                <div class="col-lg-1 col-md-6"></div>
+            </div>
 
-                        <div class="row">
-                            <div class="col-lg-1 col-md-6"></div>
-                            <div class="col-lg-3 col-6">
-                                <div class="small-box bg-info">
-                                <div class="inner">
-                                    <h3>{{$data1-$data6}}</h3>
-                                    <p>รถยนต์ทั้งหมด</p>
-                                </div>
-                                <div class="icon">
-                                    <i class="fa fa-car fa-5x"></i>
-                                </div>
-                                    <a href="{{ route('datacar', 1) }}" class="small-box-footer">ดูเพิ่มเติม <i class="fas fa-arrow-circle-right"></i></a>
-                                </div>
-                            </div>
-                            <div class="col-lg-4 col-6">
-                                <div class="small-box bg-purple">
-                                <div class="inner">
-                                    <h3>{{$data2}}</h3>
-                                    <p>รถยนต์ระหว่างทำสี</p>
-                                </div>
-                                <div class="icon">
-                                    <i class="fa fa-paint-brush fa-5x"></i>
-                                </div>
-                                    <a href="{{ route('datacar', 2) }}" class="small-box-footer">ดูเพิ่มเติม <i class="fas fa-arrow-circle-right"></i></a>
-                                </div>
-                            </div>
-                            <div class="col-lg-3 col-6">
-                                <div class="small-box bg-navy">
-                                <div class="inner">
-                                    <h3>{{$data3}}</h3>
-                                    <p>รถยนต์รอซ่อม</p>
-                                </div>
-                                <div class="icon">
-                                <i class="fas fa-car-crash"></i>
-                                    <!-- <i class="fa fa-exclamation-circle fa-5x"></i> -->
-                                </div>
-                                    <a href="{{ route('datacar', 3) }}" class="small-box-footer">ดูเพิ่มเติม <i class="fas fa-arrow-circle-right"></i></a>
-                                </div>
-                            </div>
-                            <div class="col-lg-1 col-md-6"></div>
-                        </div>
-
-
-                        <div class="row">
-                            <div class="col-lg-4 col-6">
-                                <div class="small-box bg-yellow">
-                                <div class="inner">
-                                    <h3><font color="white">{{$data4}}</font></h3>
-                                    <p><font color="white">รถยนต์ระหว่างซ่อม</font></p<font>
-                                </div>
-                                <div class="icon">
-                                    <i class="fa fa-wrench fa-5x"></i>
-                                </div>
-                                    <a href="{{ route('datacar', 4) }}" class="small-box-footer"><font color="white">ดูเพิ่มเติม <i class="fas fa-arrow-circle-right"></i></font></a>
-                                </div>
-                            </div>
-                            <div class="col-lg-4 col-6">
-                                <div class="small-box bg-green">
-                                <div class="inner">
-                                    <h3>{{$data5}}</h3>
-                                    <p>รถยนต์พร้อมขาย</p>
-                                </div>
-                                <div class="icon">
-                                    <i class="fab fa-bitcoin"></i>
-                                </div>
-                                    <a href="{{ route('datacar', 5) }}" class="small-box-footer">ดูเพิ่มเติม <i class="fas fa-arrow-circle-right"></i></a>
-                                </div>
-                            </div>
-                            <div class="col-lg-4 col-6">
-                                <div class="small-box bg-red">
-                                <div class="inner">
-                                    <h3>{{$data6}}</h3>
-                                    <p>รถยนต์ที่ขายแล้ว</p>
-                                </div>
-                                <div class="icon">
-                                    <i class="fas fa-cart-arrow-down"></i>
-                                </div>
-                                    <a href="{{ route('datacar', 6) }}" class="small-box-footer">ดูเพิ่มเติม <i class="fas fa-arrow-circle-right"></i></a>
-                                </div>
-                            </div>
-                        </div>
+            <div class="row">
+                <div class="col-lg-4 col-6">
+                    <div class="small-box bg-yellow">
+                    <div class="inner">
+                        <h3><font color="white">{{$data4}}</font></h3>
+                        <p><font color="white">รถยนต์ระหว่างซ่อม</font></p<font>
+                    </div>
+                    <div class="icon">
+                        <i class="fa fa-wrench fa-5x"></i>
+                    </div>
+                        <a href="{{ route('datacar', 4) }}" class="small-box-footer"><font color="white">ดูเพิ่มเติม <i class="fas fa-arrow-circle-right"></i></font></a>
+                    </div>
+                </div>
+                <div class="col-lg-4 col-6">
+                    <div class="small-box bg-green">
+                    <div class="inner">
+                        <h3>{{$data5}}</h3>
+                        <p>รถยนต์พร้อมขาย</p>
+                    </div>
+                    <div class="icon">
+                        <i class="fab fa-bitcoin"></i>
+                    </div>
+                        <a href="{{ route('datacar', 5) }}" class="small-box-footer">ดูเพิ่มเติม <i class="fas fa-arrow-circle-right"></i></a>
+                    </div>
+                </div>
+                <div class="col-lg-4 col-6">
+                    <div class="small-box bg-red">
+                    <div class="inner">
+                        <h3>{{$data6}}</h3>
+                        <p>รถยนต์ที่ขายแล้ว</p>
+                    </div>
+                    <div class="icon">
+                        <i class="fas fa-cart-arrow-down"></i>
+                    </div>
+                        <a href="{{ route('datacar', 6) }}" class="small-box-footer">ดูเพิ่มเติม <i class="fas fa-arrow-circle-right"></i></a>
                     </div>
                 </div>
             </div>
         </div>
-
     </div>
 
     <div class="modal fade" id="modal-1">
@@ -148,14 +126,13 @@
     </script>
 
      {{-- Popup --}}
-  <script>
-    $(function () {
-      $("#modal-1").on("show.bs.modal", function (e) {
-        var link = $(e.relatedTarget).data("link");
-        $("#modal-1 .modal-body").load(link, function(){
+    <script>
+        $(function () {
+        $("#modal-1").on("show.bs.modal", function (e) {
+            var link = $(e.relatedTarget).data("link");
+            $("#modal-1 .modal-body").load(link, function(){
+            });
         });
-      });
-    });
-  </script>
-
+        });
+    </script>
 @endsection
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,7 +8,7 @@
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
     <!-- <title>{{ config('app.name', 'Laravel') }}</title> -->
-    <title>ชูเกียรติ รถบ้าน</title>
+    <title>CHOOKIAT HOMECAR</title>
     <link rel="icon" href="{{ asset('dist/img/homecar.png') }}" type="image/ico" />
 
     <!-- Scripts -->
@@ -21,19 +21,18 @@
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 </head>
-<!-- <body style="background-color: #FF8C00;"> -->
 <body>
     <div id="app">
-        <nav class="navbar navbar-expand-md navbar-light navbar-laravel">
+        <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
             <div class="container">
                 <a class="navbar-brand" href="{{ url('/') }}">
-                  Chookiat Pattani
+                  {{-- Chookiat Pattani --}}
                     <!-- {{ config('app.name', 'Laravel') }} -->
                 </a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
                 </button>
-
+                
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                     <!-- Left Side Of Navbar -->
                     <ul class="navbar-nav mr-auto">
@@ -44,12 +43,12 @@
                     <ul class="navbar-nav ml-auto">
                         <!-- Authentication Links -->
                         @guest
-                            <!--<li class="nav-item">
-                                <a class="nav-link" href="{{ route('login') }}"><img class="img-responsive" src="{{ asset('dist/img/login2.jpg') }}" alt="User Image" style = "width: 150px"></a>
-                            </li>-->
+                            <li class="nav-item">
+                                <a class="nav-link text-white" href="{{ route('login') }}">{{ __('Login') }}</a>
+                            </li>
                             @if (Route::has('register'))
                                 <li class="nav-item">
-                                    <!-- <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a> -->
+                                    <a class="nav-link text-white" href="{{ route('register') }}">{{ __('Register') }}</a>
                                 </li>
                             @endif
                         @else
